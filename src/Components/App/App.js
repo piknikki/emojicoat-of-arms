c7f9f1cc5b2741395d6906c92a37e7fa57097cfc
Nikki <piknik@me.com>
2021-04-20 12:50:04 -0600

    Do not need home component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
 import './App.css';
-import Home from "../Home/Home";
+import NavBar from '../NavBar/NavBar';
 
 function App() {
+  // class component, holds state
+  // api calls
+  // router
+  // array to hold saved coat of arms
+  // current coat
+
   return (
-    <Home />
-  );
+    <div className="app">
+      <NavBar />
+
+      <h1>Home of Emoji Coat of Arms</h1>
+      <p>Landing page</p>
+    </div>
+  )
 }
 
 export default App;
