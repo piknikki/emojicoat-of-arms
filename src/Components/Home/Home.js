d61cbb3d729bc59e0b72e6c981a6a856fe4dcb3c
Nikki <piknik@me.com>
2021-04-21 07:29:17 -0600

    Add some styles and working on img wrapper

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import example from '../../Assets/example.png'
 import './Home.css';
 
 const Home = () => {
@@ -6,9 +7,16 @@ const Home = () => {
 
   return (
     <div className="home">
-      <p>Now you can build your family crest with emojis.
-        Yes, it's the one thing you didn't know you needed. Until now.
-      </p>
+      <div className="upper-container">
+        <p className="intro">Now you can build your family crest with emojis.
+          Yes, it's the one thing you didn't know you needed. Until now.
+        </p>
+        <div className="img-wrapper">
+          <img src={example} className="example" alt="example emoji coat of arms"/>
+        </div>
+
+      </div>
+
       <button>Get Started Now</button>
     </div>
   )
