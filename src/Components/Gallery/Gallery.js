44977f6642cb968b138129d2d545f0b60c19cdc9
Nikki <piknik@me.com>
2021-04-22 07:03:44 -0600

    Fix spacing in gallery

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../CreateCoatOfArms/CreateCoatOfArms.css'
+// import '../CreateCoatOfArms/CreateCoatOfArms.css'
+import './Gallery.css'
 import shield from "../../Assets/shield1.png";
 import Emojis from "../Emojis/Emojis";
 
@@ -16,18 +17,20 @@ const Gallery = ({ savedCoats, clickable }) => {
   })
 
   return (
-    <div className="cards-container">
-      <h3>This is where the coats should be</h3>
-
-      <div className="img-wrapper" style={{backgroundImage: `url(${shield})`}}>
-        <div className="emoji-wrapper">
-          {savedCoats &&
+    <div>
+      <h1 className="gallery-header">This is where the coats should be</h1>
+      <div className="gallery-cards-container">
+        <div className="gallery-img-wrapper" style={{backgroundImage: `url(${shield})`}}>
+          <div className="gallery-emoji-wrapper">
+            {savedCoats &&
             mappedCoats
-          }
+            }
+          </div>
         </div>
-      </div>
 
+      </div>
     </div>
+
   )
 }
 
