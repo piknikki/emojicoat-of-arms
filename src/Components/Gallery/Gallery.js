1b8f18165fe749fd26c9b69e5a460fd43611d638
Nikki <piknik@me.com>
2021-04-24 07:16:51 -0600

    Add message to gallery if no coats are made yet

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import '../CreateCoatOfArms/CreateCoatOfArms.css'
 import Coats from "../Coats/Coats";
+import { Link } from "react-router-dom";
 
 const Gallery = ({ savedCoats }) => {
 
   return (
-    <Coats coats={savedCoats}/>
+    <div className="gallery-feedback">
+      {savedCoats ? <Coats coats={savedCoats}/> :
+        <div>
+          <h3>Looks like you've got a clean slate to make something great! </h3>
+          <Link to="/create" >
+            <button className="gallery-start-btn">
+              Get Started Now
+              <span className="toolbox">🧰</span>
+            </button>
+          </Link>
+        </div>}
+    </div>
   )
 }
 
